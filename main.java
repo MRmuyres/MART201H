22ae31f260da931050c8dc2bfd2bb3d479612ad2
MRmuyres <157074061+MRmuyres@users.noreply.github.com>
2025-11-07 19:40:31 -0700

    Update main.java

---
+++ main.java
@@ -0,0 +1,27 @@
+// variables
+const cures = ['Sunshine',
+'Take a walk',
+'Smile more',
+'Yoga',
+'Think happy thoughts',
+'Affirmations',
+'Dieting',
+'Positive attitude',
+'Drink water',
+'Dont eat spices',
+'Box breathing',
+'Apple cider vinegar',
+'Take more vitamins',
+'St. Johns Wort'];
+
+
+function getRandomPrompt() {
+  const randomIndex = Math.floor(Math.random() * cures.length);
+  return cures[randomIndex];
+}
+
+// Add click event listener to the image button
+document.getElementById('pillButton').addEventListener('click', function() {
+  const prompt = getRandomPrompt();
+  alert(prompt);
+});
